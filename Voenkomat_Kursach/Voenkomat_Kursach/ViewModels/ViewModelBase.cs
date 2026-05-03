c323f2d73dba3d9023550988127ee82d7e7b5512
ches <chesnigitpochta@gmail.com>
2026-05-04 09:57:36 +1000

    добавил проект rider'а

---
+++ Voenkomat_Kursach/Voenkomat_Kursach/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Voenkomat_Kursach.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Voenkomat_Kursach/Voenkomat_Kursach/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Voenkomat_Kursach.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
