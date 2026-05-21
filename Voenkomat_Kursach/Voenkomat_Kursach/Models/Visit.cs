5d66a1f2f47acbc4a39c6c68f82e549e1613cf09
ches <chesnigitpochta@gmail.com>
2026-05-21 11:24:52 +1000

    у нас теперь есть визит

---
+++ Voenkomat_Kursach/Voenkomat_Kursach/Models/Visit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Voenkomat_Kursach.Models;
+
+public class Visit
+{
+    
+    public int Id { get; set; }
+    public Dude Dude { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly InTime { get; set; }
+    public string Goal { get; set; }
+    public TimeOnly OutTime { get; set; }
+
+
+    public Visit()
+    {
+        Id = -1;
+        Dude = new();
+        Date = DateOnly.FromDateTime(DateTime.Now);
+        InTime = TimeOnly.FromDateTime(DateTime.Now);
+        Goal = "";
+        OutTime = TimeOnly.FromDateTime(DateTime.Now);
+    }
+    
+    public Visit(int id, Dude dude, DateOnly date, TimeOnly inTime, string goal, TimeOnly outTime)
+    {
+        Id = id;
+        Dude = dude;
+        Date = date;
+        InTime = inTime;
+        Goal = goal;
+        OutTime = outTime;
+    }
+    
+}
