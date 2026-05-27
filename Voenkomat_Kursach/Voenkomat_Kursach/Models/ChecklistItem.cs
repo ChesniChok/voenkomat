078b1a0fd9056e97e520d53b8b99118b77a85e74
ches <chesnigitpochta@gmail.com>
2026-05-27 16:46:12 +1000

    в схеме и моделях появилась сущность записи в чеклисте

---
+++ Voenkomat_Kursach/Voenkomat_Kursach/Models/ChecklistItem.cs
@@ -0,0 +1,28 @@
+﻿namespace Voenkomat_Kursach.Models;
+
+public class ChecklistItem
+{
+    
+    public int Id { get; set; }
+    public Job Doctor { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+
+    public ChecklistItem()
+    {
+        Id = -1;
+        Doctor = new();
+        Name = "";
+        Description = "";
+    }
+    
+    public ChecklistItem(int id, Job doctor, string name, string description)
+    {
+        Id = id;
+        Doctor = doctor;
+        Name = name;
+        Description = description;
+    }
+    
+}
