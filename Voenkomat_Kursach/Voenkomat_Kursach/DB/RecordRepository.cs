418b8ff319a724b69eac704d0db5910acb0be275
ches <chesnigitpochta@gmail.com>
2026-06-21 16:39:48 +1000

    окно главврача готово (наверное, скорее нет)

--- Voenkomat_Kursach/Voenkomat_Kursach/DB/RecordRepository.cs
+++ Voenkomat_Kursach/Voenkomat_Kursach/DB/RecordRepository.cs
@@ -54,16 +54,97 @@ public class RecordRepository : BaseRepository<Record>
             return records;
     }
     
-    public List<Record> GetAllTyped(string type)
+    public List<Record> GetAllTyped(MedComission m, string type)
     {
         List<Record> records = new List<Record>();
         try
         {
             OpenConnection();
-            string sql = "SELECT * FROM records where Type = @type";
+            string sql = "select \n\t*\nfrom\n\trecords r \n\tjoin \n\tmedcomissions m \n\ton\n\t\tr.MedComission_Id = m.Id\nwhere \n\tm.Id = @mid\n\tand \n\tr.type = @typ\n;";
             using (var mc = new MySqlCommand(sql, _connection))
             using (var dr = mc.ExecuteReader())
             {
+                mc.Parameters.AddWithValue("@mid", m.Id);
+                mc.Parameters.AddWithValue("@typ", type);
+                while (dr.Read())
+                {
+                    int medComiddionId = dr.GetInt32("MedComission_Id");
+                    int employeeId = dr.GetInt32("Employee_Id");
+                        
+                    records.Add(new Record(
+                        dr.GetInt32("Id"),
+                        dr.GetString("Type"),
+                        _employeeRepository.GetById(employeeId),
+                        _medComissionRepository.GetById(medComiddionId),
+                        dr.GetString("Content"),
+                        dr.GetString("Description")
+                    ));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+                
+        }
+        finally
+        {
+            CloseConnection();
+        }
+        return records;
+    }
+    
+    public List<Record> GetAllMed(MedComission m)
+    {
+        List<Record> records = new List<Record>();
+        try
+        {
+            OpenConnection();
+            string sql = "select \n\t*\nfrom\n\trecords r \n\tjoin \n\tmedcomissions m \n\ton\n\t\tr.MedComission_Id = m.Id\nwhere \n\tm.Id = @mid\n;";
+            using (var mc = new MySqlCommand(sql, _connection))
+            using (var dr = mc.ExecuteReader())
+            {
+                mc.Parameters.AddWithValue("@mid", m.Id);
+                while (dr.Read())
+                {
+                    int medComiddionId = dr.GetInt32("MedComission_Id");
+                    int employeeId = dr.GetInt32("Employee_Id");
+                        
+                    records.Add(new Record(
+                        dr.GetInt32("Id"),
+                        dr.GetString("Type"),
+                        _employeeRepository.GetById(employeeId),
+                        _medComissionRepository.GetById(medComiddionId),
+                        dr.GetString("Content"),
+                        dr.GetString("Description")
+                    ));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+                
+        }
+        finally
+        {
+            CloseConnection();
+        }
+        return records;
+    }
+    
+    public List<Record> GetAllTyped(string type, Job doctor)
+    {
+        List<Record> records = new List<Record>();
+        try
+        {
+            OpenConnection();
+            string sql = "select \n\t* \nfrom \n\trecords r \n\tjoin \n\temployees e  \n\ton\n\t\tr.Author_Id = e.Id \n\tjoin\n\tjobs j \n\ton\n\t\te.Job_Id = j.Id\nwhere \n\tr.Type = @type\n\tand\n\tj.Id = @jid\n;";
+            using (var mc = new MySqlCommand(sql, _connection))
+            using (var dr = mc.ExecuteReader())
+            {
+                mc.Parameters.AddWithValue("@type", type);
+                mc.Parameters.AddWithValue("@jid", doctor.Id);
                 while (dr.Read())
                 {
                     int medComiddionId = dr.GetInt32("MedComission_Id");
--- Voenkomat_Kursach/Voenkomat_Kursach/ViewModels/ComissionViewModel.cs
+++ Voenkomat_Kursach/Voenkomat_Kursach/ViewModels/ComissionViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Voenkomat_Kursach.DB;
 using Voenkomat_Kursach.Models;
 
@@ -10,31 +13,146 @@ namespace Voenkomat_Kursach.ViewModels;
 public partial class ComissionViewModel : MedWorkersViewModel
 {
 
+    private MedComissionRepository _mr;
     private RecordRepository _rr;
+
+    private MedComission _currentMedcomission;
     
     public ComissionViewModel(IServiceProvider sp, MedComissionRepository mr, RecordRepository rr, User user, Window win, Recruit rec) : base(sp, mr, user, win, rec)
     {
+        
+        _mr = mr;
         _rr = rr;
+
+        _currentMedcomission = _mr.GetRecsLast(rec);
+        
+        GetAllProperties();
+
     }
 
 
-    private void GetChecks()
+    private List<Record> _allRecords;
+    private void GetRecords() => _allRecords = _rr.GetAllMed(_currentMedcomission);
+
+    private List<Record> _allChecks;
+    private List<Record> _allAdds;
+    private List<Record> _allConclusions;
+    private void GetChecks() => _allRecords.Where(r => r.Type == "осмотр");
+    private void GetAdds() => _allRecords.Where(r => r.Type == "дополнение");
+    private void GetConclusions() => _allRecords.Where(r => r.Type == "заключение");
+    
+    
+    
+    [ObservableProperty] private ObservableCollection<Record> _terChecks;
+    [ObservableProperty] private ObservableCollection<Record> _terAdds;
+    [ObservableProperty] private Record _terConclusion;
+    private void SetTerProperties()
     {
-
-        HirChecks = new(_rr.GetAllTyped("хирург осмотр"));
-
+        var jobName = "терапевт";
+        
+        TerChecks = new(_allChecks.Where(c => c.Author.Job.Name == jobName));
+        TerAdds = new(_allAdds.Where(а => а.Author.Job.Name == jobName));
+        TerConclusion = _allConclusions.First(c => c.Author.Job.Name == jobName);
     }
-
-    private void GetAdds()
+    
+    [ObservableProperty] private ObservableCollection<Record> _otorChecks;
+    [ObservableProperty] private ObservableCollection<Record> _otorAdds;
+    [ObservableProperty] private Record _otorConclusion;
+    private void SetOtorProperties()
     {
-
-        HirAdds = new(_rr.GetAllTyped("хирург дополнение"));
-
+        var jobName = "оториноларинголог";
+        
+        OtorChecks = new(_allChecks.Where(c => c.Author.Job.Name == jobName));
+        OtorAdds = new(_allAdds.Where(а => а.Author.Job.Name == jobName));
+        OtorConclusion = _allConclusions.First(c => c.Author.Job.Name == jobName);
     }
-
-
+    
+    [ObservableProperty] private ObservableCollection<Record> _psihChecks;
+    [ObservableProperty] private ObservableCollection<Record> _psihAdds;
+    [ObservableProperty] private Record _psihConclusion;
+    private void SetPsihProperties()
+    {
+        var jobName = "психиатр";
+        
+        PsihChecks = new(_allChecks.Where(c => c.Author.Job.Name == jobName));
+        PsihAdds = new(_allAdds.Where(а => а.Author.Job.Name == jobName));
+        PsihConclusion = _allConclusions.First(c => c.Author.Job.Name == jobName);
+    }
+    
+    [ObservableProperty] private ObservableCollection<Record> _nevrChecks;
+    [ObservableProperty] private ObservableCollection<Record> _nevrAdds;
+    [ObservableProperty] private Record _nevrConclusion;
+    private void SetNevrProperties()
+    {
+        var jobName = "невролог";
+        
+        NevrChecks = new(_allChecks.Where(c => c.Author.Job.Name == jobName));
+        NevrAdds = new(_allAdds.Where(а => а.Author.Job.Name == jobName));
+        NevrConclusion = _allConclusions.First(c => c.Author.Job.Name == jobName);
+    }
+    
     [ObservableProperty] private ObservableCollection<Record> _hirChecks;
     [ObservableProperty] private ObservableCollection<Record> _hirAdds;
+    [ObservableProperty] private Record _hirConclusion;
+    private void SetHirProperties()
+    {
+        var jobName = "хирург";
+        
+        HirChecks = new(_allChecks.Where(c => c.Author.Job.Name == jobName));
+        HirAdds = new(_allAdds.Where(а => а.Author.Job.Name == jobName));
+        HirConclusion = _allConclusions.First(c => c.Author.Job.Name == jobName);
+    }
+    
+    [ObservableProperty] private ObservableCollection<Record> _stomChecks;
+    [ObservableProperty] private ObservableCollection<Record> _stomAdds;
+    [ObservableProperty] private Record _stomConclusion;
+    private void SetStomProperties()
+    {
+        var jobName = "стоматолог";
+        
+        StomChecks = new(_allChecks.Where(c => c.Author.Job.Name == jobName));
+        StomAdds = new(_allAdds.Where(а => а.Author.Job.Name == jobName));
+        StomConclusion = _allConclusions.First(c => c.Author.Job.Name == jobName);
+    }
+    
+    [ObservableProperty] private ObservableCollection<Record> _okulChecks;
+    [ObservableProperty] private ObservableCollection<Record> _okulAdds;
+    [ObservableProperty] private Record _okulConclusion;
+    private void SetOkulProperties()
+    {
+        var jobName = "окулист";
+        
+        OkulChecks = new(_allChecks.Where(c => c.Author.Job.Name == jobName));
+        OkulAdds = new(_allAdds.Where(а => а.Author.Job.Name == jobName));
+        OkulConclusion = _allConclusions.First(c => c.Author.Job.Name == jobName);
+    }
+
+    private void GetAllProperties()
+    {
+        
+        GetRecords();
+        
+        GetChecks();
+        GetAdds();
+        GetConclusions();
+        
+        SetTerProperties();
+        SetPsihProperties();
+        SetOtorProperties();
+        SetHirProperties();
+        SetStomProperties();
+        SetOkulProperties();
+        
+    }
+
+    [RelayCommand]
+    public void FinishHim()
+    {
+        
+        _currentMedcomission.EndDate = DateOnly.FromDateTime(DateTime.Now);
+        
+    }
+    
 
 
     protected override void GoBack() => GoToChoose();
